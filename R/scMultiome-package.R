34182bd0f68555251f92e5651ddeac98b76abb6a
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2022-09-06 04:45:13 -0700

    modify package doc

--- R/scMultiome-package.R
+++ R/scMultiome-package.R
@@ -1,8 +1,11 @@
 #' @details
 #' Single cell multiome data sets, paired and unpaired, were analyzed
 #' with the \code{ArchR} package in order to obtain epiregulons.
-#' \code{ArchR} projects were converted to \code{MultiAssayExperiment}
-#' objects and serialized as .RDS files.
+#' \code{ArchR} projects were converted to \code{MultiAssayExperiment} objects.
+#'
+#' The creation of all datasets is described in detail in respective help files.
+#' Run \code{listDatasets()} to view a list of available data sets or see \code{Datasets} below.
+#' See \code{?<DATASET_NAME>} for details on particular data sets, e.g. \code{?prostateENZ}.
 #'
 #' @section Datasets:
 #' \itemize{
@@ -12,11 +15,5 @@
 #'
 #' }
 #'
-
 #' @keywords internal
 "_PACKAGE"
-
-
-## usethis namespace: start
-## usethis namespace: end
-NULL
