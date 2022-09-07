3ad3062f9fc44d1ca250f8c7bc94723732b1f0ed
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2022-09-07 08:18:14 -0700

    update docs

--- R/determineDataset.R
+++ R/determineDataset.R
@@ -4,10 +4,12 @@
 #' Obtain name of dataset from the (grand)parent call.
 #'
 #' This function reads the name of the function two steps up the call stack and
-#' returns is as a string. It is called by \code{loadSCE} and \code{getMetadata}
+#' returns is as a string. It is called by \code{loadExp} and \code{getMetadata}
 #' in order to determine the multiome dataset being queried.
 #'
 #' @return A character string reflecting the name of the dataset.
+#'
+#' @keywords internal
 #'
 determineDataset <- function() {
     deparse(sys.call(-2L)[[1]])
