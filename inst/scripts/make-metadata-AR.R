91d8d96691a015784302ff9e157c58b0d4afd7f7
Tomasz Wlodarczyk <tomwlo@gmail.com>
2024-09-09 07:01:15 -0700

    AR metadata

---
+++ inst/scripts/make-metadata-AR.R
@@ -0,0 +1,36 @@
+
+# ExperimentHub metadata
+# see ?ExperimentHubData::makeExperimentHubMetadata for details
+metadata.dataset <-
+  data.frame(
+    Title = "Response of prostate cancer cells to drug treatment",
+    Description = "Gene expression and ATACseq data from 6 cell lines (LNCaP, VCaP, MDA, 22Rv1, DU145, H660) after treatment with AR-targeting drugs",
+    BiocVersion = "3.21",
+    Genome = "hg38",
+    SourceType = ".h5, fragment files",
+    SourceUrl = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE251977",
+    SourceVersion = "Sep 09 2024",
+    Species = "Homo sapiens",
+    TaxonomyId = "9606",
+    Coordinate_1_based = TRUE,
+    DataProvider = "Genentech, Inc.",
+    Maintainer = desc::desc_get_maintainer(),
+    RDataClass = "MultiAssayExperiment",
+    DispatchClass = "FilePath",
+    RDataPath = "scMultiome/AR.h5"
+)
+
+# dataset manifest metadata
+# see ?listDatasets for details
+manifest.dataset <-
+  data.frame(
+    Call = "AR()",
+    Author = "Yao",
+    Title = "Response of prostate cancer cells to drug treatment",
+    Species = "Homo sapiens",
+    Lineage = "Prostate",
+    CellNumber = "23118",
+    Multiome = "paired",
+    DiskSize = "3.3 GB",
+    Version = "2024-09-09"
+)
