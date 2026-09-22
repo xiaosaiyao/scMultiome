1dcba85e91938ef8079f3635d38cc09aa9979f0b
Tomasz Włodarczyk <tomasz.wlodarczyk@contractors.roche.com>
2026-09-22 09:08:28 +0000

    Revert "do not use names to avoid extra metadata column in the reconstructed GRanges object"
    This reverts commit f1db6cabfb2631dc2bec49697ff7c50238e84bcd.

--- tests/testthat/test-GRtools.R
+++ tests/testthat/test-GRtools.R
@@ -5,18 +5,17 @@
 
 gr0 <- GenomicRanges::GRanges(
     "seqnames" = S4Vectors::Rle(c("chr1", "chr1", "chr2", "chr3"), c(1, 3, 2, 4)),
-    "ranges" = IRanges::IRanges(1:10, width = 10:1),
-    #"ranges" = IRanges::IRanges(1:10, width = 10:1),
+    "ranges" = IRanges::IRanges(1:10, width = 10:1, names = head(letters, 10)),
     "strand" = S4Vectors::Rle(BiocGenerics::strand(c("-", "+", "*", "+", "-")), c(1, 2, 2, 3, 2)),
     "score" = 1:10,
     "GC" = seq(1, 0, length = 10))
 
 
 # prepare for storage
-gr0df <- scMultiome:::storeGR(gr0)
+gr0df <- storeGR(gr0)
 
 # restore from storage
-GR0 <- scMultiome:::restoreGR(gr0df)
+GR0 <- restoreGR(gr0df)
 
 test_that("conversion of GR to data.frame", {
     expect_s3_class(gr0df, "data.frame")
